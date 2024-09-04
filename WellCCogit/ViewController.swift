fc55bd234db97a3308373b70476afb0ca9637a07
junho7108 <yyjh7108@gmail.com>
2024-09-04 19:22:30 +0900

    Set up Xcode project

---
+++ WellCCogit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WellCCogit
+//
+//  Created by Junho Yoon on 9/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
