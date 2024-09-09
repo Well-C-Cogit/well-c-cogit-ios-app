4fb51cc73638ece9aa64041bdcf5c7c7725ce069
junho7108 <yyjh7108@gmail.com>
2024-09-09 21:27:13 +0900

    feat: Implement ConfigurableUI Protocol

--- WellCCogit/Protocol/ConfigurableUI.swift
+++ WellCCogit/Protocol/ConfigurableUI.swift
@@ -5,4 +5,19 @@
 //  Created by Junho Yoon on 9/9/24.
 //
 
-import Foundation
+import UIKit
+
+protocol ConfigurableUI where Self: UIView {
+    associatedtype BaseView: UIView
+    
+    var baseView: BaseView { get set }
+    
+    func configureBaseView()
+}
+
+extension ConfigurableUI {
+    func configureBaseView() {
+        self.addSubview(self.baseView)
+        baseView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+}
